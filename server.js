94bed6eb9f61ca5fb47a8d508d636f2bf0e93f23
jbrolex42-cell <jbrolex42@gmail.com>
2026-02-27 11:27:29 +0300

    Production safe server rewrite

--- server.js
+++ server.js
@@ -18,17 +18,17 @@ const app = express();
 
 app.use(helmet());
 
-const FRONTEND_ORIGIN = "https://emergency-frontend-sepia.vercel.app";
+const FRONTEND_ORIGIN = process.env.FRONTEND_URL;
 
 app.use((req, res, next) => {
 
-    res.header("Access-Control-Allow-Origin", FRONTEND_ORIGIN);
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Origin", FRONTEND_ORIGIN);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
     if (req.method === "OPTIONS") {
-        return res.sendStatus(204);
+        return res.sendStatus(200);
     }
 
     next();
