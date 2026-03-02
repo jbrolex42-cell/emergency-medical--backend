253c6c18568f4b95fedfc0a9dd0f7d1b0045a222
jbrolex42-cell <jbrolex42@gmail.com>
2026-03-02 08:30:04 +0300

    update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
-
+app.set('trust proxy', 1);
 app.use(
   morgan(process.env.NODE_ENV === "production" ? "combined" : "dev")
 );
