61761a38f19b9108bebdf6c46937c558d26191e3
jbrolex42-cell <jbrolex42@gmail.com>
2026-03-05 10:37:29 +0300

    Fix auth route duplication and validator import

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,16 +5,24 @@ const authController = require("../controllers/authController");
 const validators = require("../validators/validator");
 const { authenticate } = require("../middleware/authMiddleware");
 
-router.post("/register", validators.authValidators.register, authController.register);
+router.post(
+  "/register",
+  validators.authValidators.register,
+  authController.register
+);
 
-router.post("/login", validators.authValidators.login, authController.login);
+router.post(
+  "/login",
+  validators.authValidators.login,
+  authController.login
+);
 
-router.get("/me",authenticate,authController.getCurrentUser);
+router.get("/me", authenticate, authController.getCurrentUser);
 
-router.put("/profile",authenticate,authController.updateProfile);
+router.put("/profile", authenticate, authController.updateProfile);
 
-router.post("/forgot-password",authController.forgotPassword);
+router.post("/forgot-password", authController.forgotPassword);
 
-router.post("/reset-password/:token",authController.resetPassword);
+router.post("/reset-password/:token", authController.resetPassword);
 
 module.exports = router;
